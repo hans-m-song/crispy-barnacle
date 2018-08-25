d35238ca44bff9000f2932b53c446788da884998
hans-m-song <hans.m.song@gmail.com>
2018-08-25 20:46:31 +1000

    added a bunch of stuff, fixing memory leaks

---
+++ common.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int get_line(char** str, int* size, FILE* f) {
+    char line[256];
+    char* ptr;
+    int len;
+    if(str == NULL || size == NULL) {
+        return -1;
+    }
+
+    if(feof(f)) {
+        return -1;
+    }
+
+    fgets(line, 256, f);
+
+    ptr = strchr(line, '\n');
+    if(ptr) {
+        *ptr = '\0';
+    }
+
+    len = strlen(line);
+
+    if((len + 1) < 256) {
+        ptr = realloc(*str, 256);
+        if(ptr == NULL) {
+            return -1;
+        }
+        *str = ptr;
+        *size = 256;
+    }
+
+    strcpy(*str, line);
+    return len;
+}
---
+++ common.h
@@ -0,0 +1,8 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdlib.h>
+
+int get_line(char** str, int* size, FILE* f);
+
+#endif
